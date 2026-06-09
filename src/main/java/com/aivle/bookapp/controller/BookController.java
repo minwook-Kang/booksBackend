6af03e5bdce37e62ed5eb45f0b9f406a9b79a012
hywznn <choihj0510@naver.com>
2026-06-09 14:30:11 +0900

    [feature/api] keyword 검색, 조회수 증가

--- src/main/java/com/aivle/bookapp/controller/BookController.java
+++ src/main/java/com/aivle/bookapp/controller/BookController.java
@@ -1,4 +1,31 @@
 package com.aivle.bookapp.controller;
 
+
+import com.aivle.bookapp.domain.Book;
+import com.aivle.bookapp.services.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+@RequiredArgsConstructor
 public class BookController {
+    private final BookService bookService;
+
+    // 도서 키워드 검색 ( 키워드 없을 시 전체 조회 )
+    @GetMapping
+    public List<Book> getBooks(@RequestParam(required = false) String q) {
+        if (q == null || q.trim().isEmpty()) {
+            return bookService.getBookList();
+        }
+        return bookService.searchBooks(q);
+    }
+
+    @PatchMapping("/{id}/views")
+    public Book increaseViewCount(@PathVariable Long id) {
+        return bookService.increaseViewCount(id);
+    }
+
 }
