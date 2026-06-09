b95059f3b4f1d982ac7456c642d41379cd9c8668
minwook-Kang <72792893+minwook-Kang@users.noreply.github.com>
2026-06-09 15:10:00 +0900

    Implement CRUD operations in BookController

--- src/main/java/com/aivle/bookapp/controller/BookController.java
+++ src/main/java/com/aivle/bookapp/controller/BookController.java
@@ -1,4 +1,40 @@
 package com.aivle.bookapp.controller;
 
+import com.aivle.bookapp.domain.Book;
+import com.aivle.bookapp.services.BookService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
 public class BookController {
+
+    private final BookService bookService;
+
+    @GetMapping("/books/{id}")
+    public Book getBook(@PathVariable Long id){
+        return bookService.findById(id);
+    }
+
+    @DeleteMapping("/books/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable Long id){
+        bookService.deleteBook(id);
+        return ResponseEntity.noContent().build();
+
+    }
+
+    @PostMapping("/books")
+    public ResponseEntity<Book> createBook(@Valid @RequestBody Book book){
+        Book saved = bookService.create(book);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+    }
+
+    @PatchMapping("/books/{id}")
+    public Book updateBook(@PathVariable Long id, @RequestBody Book book){
+        return bookService.update(id, book);
+    }
 }
