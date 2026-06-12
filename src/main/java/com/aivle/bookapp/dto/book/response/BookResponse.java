31e476e45e235d17738c7b215fb4cd193e3ba811
JoJimi <duddnddnfl@naver.com>
2026-06-12 11:09:31 +0900

    refactor: 최신순/오래된순 정렬 리펙토링

--- src/main/java/com/aivle/bookapp/dto/book/response/BookResponse.java
+++ src/main/java/com/aivle/bookapp/dto/book/response/BookResponse.java
@@ -2,19 +2,23 @@ package com.aivle.bookapp.dto.book.response;
 
 import com.aivle.bookapp.domain.Book;
 
+import java.time.LocalDateTime;
+
 public record BookResponse(
-        Long id, String title, String author, String content,
-        String genre, String coverImageUrl, Integer views, Integer likes
+        Long id, String title, String author,
+        String genre, String coverImageUrl, Integer views, Integer likes,
+        LocalDateTime createdAt, LocalDateTime updatedAt
 ) {
     public static BookResponse from(Book book) {
         return new BookResponse(
                 book.getId(),
                 book.getTitle(),
                 book.getAuthor(),
-                book.getContent(),
                 book.getGenre().getName(),
                 book.getCoverImageUrl(),
                 book.getViews(),
-                book.getLikes());
+                book.getLikes(),
+                book.getCreatedAt(),
+                book.getUpdatedAt());
     }
 }
